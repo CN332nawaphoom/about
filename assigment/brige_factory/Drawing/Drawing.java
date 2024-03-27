85d1c48a54cb2d22af6b3dbaa19be1abfed97148
suttha jongjaroen <6410615121@cn330>
2024-03-27 15:59:57 +0700

    init Drawing

---
+++ assigment/brige_factory/Drawing/Drawing.java
@@ -0,0 +1,9 @@
+package Drawing;
+
+import Shape.Shape;
+
+public interface Drawing {
+    public void draw(Shape s);
+
+    public void fill(Shape s);
+}
---
+++ assigment/brige_factory/Drawing/DrawingAWT.java
@@ -0,0 +1,19 @@
+package Drawing;
+
+import Shape.Shape;
+
+public class DrawingAWT implements Drawing{
+
+    @Override
+    public void draw(Shape s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public void fill(Shape s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'fill'");
+    }
+    
+}
