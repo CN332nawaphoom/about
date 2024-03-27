c971509661e4e2e2c0fef918a95f9ede3b345010
suttha jongjaroen <6410615121@cn330>
2024-03-27 15:55:54 +0700

    init Shape and its subclass

---
+++ assigment/brige_factory/Shape/Circle.java
@@ -0,0 +1,41 @@
+package Shape;
+
+public class Circle extends Shape {
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public void fill() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'fill'");
+    }
+
+    @Override
+    public String get_line_color() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get_line_color'");
+    }
+
+    @Override
+    public void set_line_color() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set_line_color'");
+    }
+
+    @Override
+    public String get_area_color() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get_area_color'");
+    }
+
+    @Override
+    public void set_area_color() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set_area_color'");
+    }
+    
+}
---
+++ assigment/brige_factory/Shape/Shape.java
@@ -0,0 +1,13 @@
+package Shape;
+
+public abstract class Shape {
+
+    public abstract void draw();
+    public abstract void fill();
+
+    public abstract String get_line_color();
+    public abstract void set_line_color();
+
+    public abstract String get_area_color();
+    public abstract void set_area_color();
+}
