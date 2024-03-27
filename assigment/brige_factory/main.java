9719e7aa75f3b2a09871b06c0e1618c29d28280f
suttha jongjaroen <6410615121@cn330>
2024-03-27 13:04:48 +0700

    update main.java

--- assigment/brige_factory/main.java
+++ assigment/brige_factory/main.java
@@ -1,16 +1,42 @@
 import java.awt.Graphics2D;
+import java.awt.Dimension;
 // import java.awt.Color;
 // import java.awt.BasicStroke;
 import java.awt.Graphics;
 
 // import java.awt.geom.Line2D;
+import javax.swing.*;
 
-// import javax.swing.JPanel;
 
+public class Main extends JPanel{
+    Main(){
+        this.setPreferredSize(new Dimension(300, 300));
+    }
 
-public class Main{
+    public static JFrame createFrame(){
+        JFrame frame = new JFrame();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Main());
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        return frame;
+    }
+
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawLine(0, 0, 300, 300);
+    }
+
     public static void main(String[] args) {
-        new MyFrame();
+        JFrame frame = createFrame();
+
+        
+        
+       
+        
 
         // try {
         //     String directoryPath = System.getProperty("user.dir");
