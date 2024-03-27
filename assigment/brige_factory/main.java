118a208fb8103feb869f9c03bead9f7273d79640
suttha jongjaroen <6410615121@cn330>
2024-03-27 15:29:18 +0700

    add saveImage method

--- assigment/brige_factory/main.java
+++ assigment/brige_factory/main.java
@@ -4,8 +4,12 @@ import java.awt.Dimension;
 // import java.awt.BasicStroke;
 import java.awt.Graphics;
 
+import javax.imageio.ImageIO;
 // import java.awt.geom.Line2D;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Main extends JPanel{
@@ -30,8 +34,23 @@ public class Main extends JPanel{
         g2d.drawLine(0, 0, 300, 300);
     }
 
+    public static void saveImage(JFrame frame,String formatname, String filePath){
+        BufferedImage image = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        frame.paint(g2d);
+        g2d.dispose();
+        try {
+            ImageIO.write(image, "PNG", new File(filePath));
+            System.out.println("Panel saved as PNG: " + filePath);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         JFrame frame = createFrame();
+        saveImage(frame,"png", "C:\\Users\\paipa\\Pictures\\test2.png");
+
 
         
         
