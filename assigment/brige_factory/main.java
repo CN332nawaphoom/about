ed0ae5f990792e1f5bc226c8143f8dc52368f084
suttha jongjaroen <6410615121@cn330>
2024-03-28 16:08:19 +0700

    update main.java

--- assigment/brige_factory/main.java
+++ assigment/brige_factory/main.java
@@ -1,18 +1,32 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class main {
     public static void main(String[] args) {
         String dir = System.getProperty("user.dir");
-        String filename = "\\configs.json";
-        String filepath = dir + filename;
+        String configName = "configs.json";
+        String filepath = dir + "\\configs\\" + configName;
+        
+        // Create directory for images if it does not exist
+        String imgDirPath = dir + "\\imgs";
+        File imgDir = new File(imgDirPath);
+        if (!imgDir.exists()) {
+            boolean success = imgDir.mkdirs();
+            if (!success) {
+                System.out.println("Failed to create directory for images.");
+                return;
+            }
+        }
 
         DrawingFactory f = new DrawingFactory(filepath);
-        try{
+        try {
             f.read_conf();
-            f.saveImage("png", "C:\\Users\\paipa\\Pictures\\test2.png");
-        }catch(FileNotFoundException e){
+
+            String imgFileName = "test2.png";
+            filepath = dir + "\\imgs\\" + imgFileName;
+            f.saveImage("png", filepath);
+        } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-        
     }
 }
